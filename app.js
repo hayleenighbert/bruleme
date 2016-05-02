12e7f8d880ddace03249ae5fa5a900baac717ecb
hayleenighbert <haylee.nigh@gmail.com>
2016-05-02 12:07:42 -0700

    Making progress

---
+++ app.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var app = angular.module('BruleApp', ['AppCtrls', "ngRoute", 'ngAnimate']);
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.config([
+	"$routeProvider", //index in the array
+	"$locationProvider", // index
+	function($routeProvider, $locationProvider) {
+		$routeProvider.when("/", { //route
+			templateUrl: "views/index.html", //render
+			controller: "AppCtrl" //provides data for that view
+		}).when("/about", {
+			templateUrl: "views/about.html"
+		}).when("/brule", {
+			templateUrl: "views/brule.html"
+		}).when("/perseus", {
+			templateUrl: "views/perseus.html",
+			controller: "PageOne"
+		}).when("/andromeda", {
+			templateUrl: "views/andromeda.html"
+		// }).when("/doughnut/:id", {
+		// 	templateUrl: "views/doughtnut.html",
+		// 	controller: "DoughnutShowCtrl"
+		}).otherwise({
+			templateUrl: "views/404.html"
+		});
+
+		$locationProvider.html5Mode(false).hashPrefix('!');
+	}
+]);
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
---
+++ js/index.js
@@ -0,0 +1,6 @@
+function randomQuote() {
+    var para = document.createElement("P");
+    var t = document.createTextNode("This is a paragraph.");
+    para.appendChild(t);
+    document.body.appendChild(para);
+}
